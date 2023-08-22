272dbfddeef3c64c8ad90110b6c3fe8099df0066
Eren OLKUN <erenolkun@gmail.com>
2023-08-22 14:25:46 +0300

    Initial Commit

---
+++ LoginViewSwiftUI/LoginViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewSwiftUIApp.swift
+//  LoginViewSwiftUI
+//
+//  Created by FERDA OLKUN on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
